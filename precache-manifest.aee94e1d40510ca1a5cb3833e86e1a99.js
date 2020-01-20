c1c3305dd9b0803f6d534ceb4e9d0a71cd4e618c
spaceowlpurrp <spaceowlpurrp@gmail.com>
2020-01-20 19:17:56 +0100

    Updates

---
+++ precache-manifest.aee94e1d40510ca1a5cb3833e86e1a99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1a3483efeb29ebcf7fb7035a40b189",
+    "url": "/LinkTaxiAppLandingPage/index.html"
+  },
+  {
+    "revision": "4ba0a4bcc9ce3f43ff6e",
+    "url": "/LinkTaxiAppLandingPage/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "86dca37788c81cdf8539",
+    "url": "/LinkTaxiAppLandingPage/static/css/main.797b152e.chunk.css"
+  },
+  {
+    "revision": "4ba0a4bcc9ce3f43ff6e",
+    "url": "/LinkTaxiAppLandingPage/static/js/2.39012652.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/LinkTaxiAppLandingPage/static/js/2.39012652.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86dca37788c81cdf8539",
+    "url": "/LinkTaxiAppLandingPage/static/js/main.5b2ffce3.chunk.js"
+  },
+  {
+    "revision": "a0aae15769f46d174ce7",
+    "url": "/LinkTaxiAppLandingPage/static/js/runtime-main.d0c08389.js"
+  },
+  {
+    "revision": "63d2b6b0e120b8707bff029a1e5e6ca3",
+    "url": "/LinkTaxiAppLandingPage/static/media/linktaxi.63d2b6b0.png"
+  }
+]);
