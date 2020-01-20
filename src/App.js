15ad83ddc2939d23c011ecc3ebcc743e642d279c
spaceowlpurrp <spaceowlpurrp@gmail.com>
2020-01-20 19:07:46 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import apple from './apple-en.png'
+import google from './google-en.png'
+import linktaxi from './linktaxi.png'
+import './App.css'
+import 'tachyons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+
+    return (
+        <div className='background bg-light-blue pa3'>
+            <article className="br3 ba bg-near-white b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+
+                <div className='pa3 center container'>
+                    <img className='pa1 pointer' src={linktaxi} alt="linktaxi"/>
+
+                    <a href="https://apps.apple.com/us/app/link-taxi/id1445721945">
+                        <img className='pa1' src={apple} alt="apple"/>
+                    </a>
+
+                    <a href="https://play.google.com/store/apps/details?id=com.tiskel.tma.wroclawlinktaxi">
+                        <img className='pa1' src={google} alt="google-play"
+                        />
+                    </a>
+                </div>
+
+            </article>
+
+        </div>
+    );
+};
 
 export default App;
+
